5620d1c4643f437eb278c190e67856f343c9d404
Adrian Irving-Beer <wisq@wisq.net>
2023-10-07 04:35:02 -0400

    WIP

--- lib/sus_bot/fetcher.ex
+++ lib/sus_bot/fetcher.ex
@@ -1,30 +1,43 @@
 defmodule SusBot.Fetcher do
+  require Logger
+
   def fetch(%URI{} = uri, timeout \\ 10000) do
-    port = open_port("yt-dlp", ["-J", URI.to_string(uri)])
-    os_pid = Port.info(port) |> Keyword.fetch!(:os_pid)
+    uri = URI.to_string(uri)
 
-    spawn_link(fn ->
-      Process.sleep(timeout)
-      System.cmd("kill", ["#{os_pid}"], stderr_to_stdout: true)
-      Process.sleep(1000)
-      System.cmd("kill", ["-9", "#{os_pid}"], stderr_to_stdout: true)
+    Task.async(fn ->
+      port = open_port("yt-dlp", ["-J", uri])
+
+      os_pid = Port.info(port) |> Keyword.fetch!(:os_pid)
+      Process.send_after(self(), {:timeout, os_pid}, timeout)
+
+      receive_loop(port)
     end)
+    |> Task.await(timeout + 1000)
+    |> then(fn
+      {:exited, 0, json} ->
+        Jason.decode(json)
 
-    case receive_loop(port, []) do
-      {0, json} -> Jason.decode(json)
-      # got sigterm
-      {143, _} -> {:error, :fetcher_timeout}
-      # got sigkill
-      {137, _} -> {:error, :fetcher_timeout}
-      # non-zero exit status
-      {n, _} when n > 0 -> {:error, :fetcher_failed}
-    end
+      {:exited, n, _} when n > 0 ->
+        Logger.error("Fetcher exited with status #{n} accessing #{inspect(uri)}")
+        {:error, :fetcher_failed}
+
+      :timed_out ->
+        Logger.warning("Fetcher timed out accessing #{inspect(uri)}")
+        {:error, :fetcher_timed_out}
+    end)
   end
 
-  defp receive_loop(port, buf) do
+  defp receive_loop(port, buf \\ []) do
     receive do
-      {^port, {:data, data}} -> receive_loop(port, [data | buf])
-      {^port, {:exit_status, n}} -> {n, Enum.reverse(buf) |> IO.iodata_to_binary()}
+      {^port, {:data, data}} ->
+        receive_loop(port, [data | buf])
+
+      {^port, {:exit_status, n}} ->
+        {:exited, n, Enum.reverse(buf) |> IO.iodata_to_binary()}
+
+      {:timeout, os_pid} ->
+        System.cmd("kill", ["-9", "#{os_pid}"], stderr_to_stdout: true)
+        :timed_out
     end
   end
 
